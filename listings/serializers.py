82a8b82233201bc5af92b2f10c552c5795fe7652
GJSayers <gemma.sayersm@gmail.com>
2023-04-21 19:28:26 +0000

    Format listings serializer

--- listings/serializers.py
+++ listings/serializers.py
@@ -35,9 +35,6 @@ class ListingSerializer(serializers.ModelSerializer):
     available_services = serializers.MultipleChoiceField(choices=SERVICES)
     slug = serializers.SlugField(required=False)
 
-    
-    
-
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
